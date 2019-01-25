de39d1b578c497f9ff82ca489d0f95b97a089071
Manish Sharma <sharmaa.manish1@gmail.com>
2019-01-25 19:39:41 +0530

    Initial commit

---
+++ AmazonAppAssignment/src/main/java/AmazonAppAssignment/AmazonAppAssignment/AmazonHomePage.java
@@ -0,0 +1,18 @@
+package AmazonAppAssignment.AmazonAppAssignment;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import java.io.File;
+
+public class AmazonHomePage {
+	//initialize the android driver
+	private static AndroidDriver<AndroidElement> androidDriver;
+	
+	public static void main(String[] args)
+	{
+		//give the path to Amazon app and launch it
+		File appPath = new File("");
+	}
+
+}
